86ba78d25b28d23bd69161827a09e2240121fa5a
Cruizard <arielmenlolo03@gmail.com>
2025-06-12 15:47:17 +0800

    cordnvim changes

--- lua/riel/plugins/cordnvim.lua
+++ lua/riel/plugins/cordnvim.lua
@@ -2,7 +2,14 @@ return {
 	"vyfor/cord.nvim",
 	build = ":Cord update",
 	opts = {
+		idle = {
+			details = "Bro doesnt know how to close vim 💀",
+			timeout = 420000,
+		},
 		text = {},
+		editing = function(opts)
+			return "Breaking " .. opts.filename
+		end,
 	},
 }
 -- vim: ts=2 sts=2 sw=2 et
